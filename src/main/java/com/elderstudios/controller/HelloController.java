efb41436ff3dc9a3ca8cafcf87e85f7e42533dc9
Adeel Mohammed <a.mohammed5832@student.leedsbeckett.ac.uk>
2017-05-01 23:46:45 +0100

    renamed

--- src/main/java/com/elderstudios/controller/HelloController.java
+++ src/main/java/com/elderstudios/controller/HelloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @Controller
-public class HelloController {
+public class HellghoController {
 
 	@Autowired
 	protected GuestBookService guestBookService;
